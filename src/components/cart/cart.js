83ecae79dec898c1e3a38cccdadcb4dd0a0ebcb7
Frank Clemente <frank.gerrard.14@gmail.com>
2023-10-12 22:51:06 -0500

    camios nuevos

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,29 +1,33 @@
 function cart() {
-
-    const ul = document.getElementById('cart_list')
-    const itemsCart = JSON.parse(localStorage.getItem('cart')) || []
+    const ul = document.getElementById("cart_list");
+    const itemsCart = JSON.parse(localStorage.getItem("cart")) || [];
 
     for (const product of itemsCart) {
-        const li = document.createElement('li')
-        li.classList.add('section__li--cart')
-        li.innerHTML += `
-        <img src=${product.image}>
-        <div>
-        <h1>${product.name}
-        </h1>
-        <h1>${product.price}.00</h1>
-        <p>${product.size}</p>
-        </div>
-        <div>
-        <di>
-        <button>-</button>
-        </di>
-        </div>
+        const li = document.createElement("li");
+        li.classList.add("section_li--cart");
+        li.innerHTML = `
+         <img src="${product.image} class="section__img--cart">
+         <div class="section__div--cart">
+           <h3 class="section__h3--cart">${product.name}</h3>
+           <h4 class="section__h4--cart"> $${product.price}.00</h4>
+           <p> ${product.zise}</p>
+         
+         <div class="section__div--buttonCart">
+         <div  class="section__div--modifed">
+         <button class="decrement">-</button>
+         <h4>${product.quantity}</h4>
+         <button class="increment">+</button>
+         </div>
+         <button class="remove">
+         <i class='bx bx-trash-alt' style='color:#ff0000'  ></i>
+         </button></div>
 
-        `
-
+         
+         </div>
+       `;
+        ul.appendChild(li);
     }
-
-
 }
+
+
 export default cart
