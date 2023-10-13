f77dd0f9d48d0778bb915a98b9f2568234f2a892
Frank Clemente <frank.gerrard.14@gmail.com>
2023-10-12 21:48:57 -0500

    cambios2

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,4 +1,29 @@
 function cart() {
 
+    const ul = document.getElementById('cart_list')
+    const itemsCart = JSON.parse(localStorage.getItem('cart')) || []
+
+    for (const product of itemsCart) {
+        const li = document.createElement('li')
+        li.classList.add('section__li--cart')
+        li.innerHTML += `
+        <img src=${product.image}>
+        <div>
+        <h1>${product.name}
+        </h1>
+        <h1>${product.price}.00</h1>
+        <p>${product.size}</p>
+        </div>
+        <div>
+        <di>
+        <button>-</button>
+        </di>
+        </div>
+
+        `
+
+    }
+
+
 }
 export default cart
